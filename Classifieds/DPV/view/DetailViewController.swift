30bfaece90099d3906199c25fc516b277059b08d
Rizwan Saleem <rizwan.saleem@dubizzle.com>
2019-10-08 12:55:46 +0400

    Added Detail View Controller

---
+++ Classifieds/DPV/view/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Classifieds
+//
+//  Created by Rizwan Saleem on 10/7/19.
+//  Copyright © 2019 Rizwan Saleem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
